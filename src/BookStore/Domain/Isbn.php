27923de4ca94b7c7160c82dfe772a029fb74a2c8
Olivier Soulet <olivier.soulet@anyti.me>
2020-11-30 13:38:45 +0100

    Add validation for ISBN

--- src/BookStore/Domain/Isbn.php
+++ src/BookStore/Domain/Isbn.php
@@ -2,12 +2,18 @@
 
 namespace App\BookStore\Domain;
 
+use App\BookStore\Domain\Exception\InvalidIsbnException;
+
 class Isbn
 {
     private string $value;
 
     public function __construct(string $value)
     {
+        if (!$this->checkIfIsbn($value)) {
+            throw new InvalidIsbnException($value);
+        }
+
         $this->value = $value;
     }
 
@@ -19,5 +25,16 @@ class Isbn
     public function toString(): string
     {
         return $this->value;
+    }
+
+    private function checkIfIsbn(string $isbn): bool
+    {
+        $regex = '/\b(?:ISBN(?:: ?| ))?((?:97[89])?\d{9}[\dx])\b/i';
+
+        if (preg_match($regex, str_replace('-', '', $isbn), $matches)) {
+            return true;
+        }
+
+        return false;
     }
 }
